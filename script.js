8b4d48c83fe0f1266df32ce0d24ff005816ab99a
LM&T <tbbragg83@gmail.com>
2020-06-14 02:20:45 -0500

    Psuedo Code finished/API calling correct city on button click

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("It's Working")
+
+
+$("button").on("click", function() {
+    var userCity = $("#user-city").val().trim();
+    var apiKey = "b15b2b973a104648ff62868774ec5427";
+     
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&appid=" + apiKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        console.log(response);
+
+    });
+
+});
