f3e2d2a26f5623a17c71c4ed50ca5168b563baba
LM&T <tbbragg83@gmail.com>
2020-06-15 02:57:45 -0500

    city search appending below but not persisting/current forecast request working and appending/ need UV-Index

--- script.js
+++ script.js
@@ -22,16 +22,32 @@ $("button").on("click", function() {
     var userCity = $("#user-city").val().trim();
     var apiKey = "b15b2b973a104648ff62868774ec5427";
      
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&appid=" + apiKey;
+    var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&appid=" + apiKey;
+    // var fiveDayQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&appid=" + apiKey;
 
     $.ajax({
-        url: queryURL,
+        url:  currentQueryURL,
         method: "GET"
     }).then(function(response) {
+
+        var cityTime = new Date(response.dt * 1000);
+        
+        $(".city").text(response.name + " (" + cityTime.toLocaleDateString("en-US") + ")");
+        $(".city").append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(".temp").text("Temperature (F) " + tempF.toFixed(2) + " °F");
+        $(".humidity").text("Humidity: " + response.main.humidity + " %");
+        $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
 
         console.log(response);
 
     });
 
+    localStorage.setItem("city", userCity);
+    var prevCity = localStorage.getItem("city");
+    $(".list-group").append("<button class='btn list-group-item'>" + prevCity + "</button>")
+
+    
+
 });
+
